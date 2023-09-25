c8c82cdd9dc4ee6a195820f57abe3b51feff88e5
manjur0 <sfmanjur21@gmail.com>
2023-09-25 23:07:17 +0600

    applied job listed

--- src/Components/AppliedJobs/AppliedJosbs.jsx
+++ src/Components/AppliedJobs/AppliedJosbs.jsx
@@ -1,19 +1,29 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import { getStoreJobApplications } from "../../Utility/LocalStorage";
 
 
 const AppliedJosbs = () => {
     const jobs = useLoaderData()
+    const [appliedJobs, setAppliedJobs] = useState([]);
     useEffect(() => {
         const storedJobsIds = getStoreJobApplications();
         if (jobs.length > 0) {
             const JobsApplied = jobs.filter(job => storedJobsIds.includes(job.id))
+            setAppliedJobs(JobsApplied);
         }
     }, [])
     return (
-        <div>
-            <h1>Applied Jobs</h1>
+        <div className="text-center my-20 text-3xl">
+            <h1 className="font-bold mb-5">Applied Jobs</h1>
+            <hr className="mb-5"/>
+            <ul>
+                {
+                    appliedJobs.map(job => <li key={job.id}>
+                        <span>{job.job_title} {job.company_name}</span>
+                    </li>)
+                }
+            </ul>
         </div>
     );
 };
